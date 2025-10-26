7379ba1359ea9c9370b3054d75927095c932866a
sidhuvardhan singavarapu <sidhuvardhans@gmail.com>
2025-10-26 16:58:55 +0530

    java project

---
+++ Table.java
@@ -0,0 +1,9 @@
+public class Table{
+public static void main(String []args){
+int a ;
+int b= 3;
+for (a=1; a<=20; a++){
+System.out.println(a +"X" + b + "=" + a*b);
+}}}
+Table tb =new Table();
+System.out.println(tb.b);
