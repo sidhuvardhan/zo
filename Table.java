fa7eee45fa5bf957daebbf8b7c54ad6dc2bc5531
sidhuvardhan singavarapu <sidhuvardhans@gmail.com>
2025-10-29 19:18:28 +0530

    ;;;;

--- Table.java
+++ Table.java
@@ -6,4 +6,9 @@ for (a=1; a<=20; a++){
 System.out.println(a +"X" + b + "=" + a*b);
 }}}
 Table tb =new Table();
+<<<<<<< HEAD
 System.out.println("helllo guru");
+=======
+System.out.println(tb.b);
+System.out.println(tb.a);
+>>>>>>> bf560c305185e6376ae921122c02540dbe0e6b72
